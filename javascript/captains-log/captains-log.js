f3f03fd385784e23d1be8cd2e84b305be6bd835d
Mario D'Andrea <goffredo2004@gmail.com>
2025-11-05 19:27:43 +0100

    javascript: Captain's Log

---
+++ javascript/captains-log/captains-log.js
@@ -0,0 +1,29 @@
+// @ts-check
+
+/**
+ * Generates a random starship registry number.
+ *
+ * @returns {string} the generated registry number.
+ */
+export function randomShipRegistryNumber() {
+  return `NCC-${Math.random() * (10000 - 1000) + 1000}`;
+}
+
+/**
+ * Generates a random stardate.
+ *
+ * @returns {number} a stardate between 41000 (inclusive) and 42000 (exclusive).
+ */
+export function randomStardate() {
+  return Math.random() * (42000 - 41000) + 41000;
+}
+
+/**
+ * Generates a random planet class.
+ *
+ * @returns {string} a one-letter planet class.
+ */
+export function randomPlanetClass() {
+  const planets = 'DHJKLMNRTY';
+  return planets[Math.floor(Math.random() * planets.length)];
+}
