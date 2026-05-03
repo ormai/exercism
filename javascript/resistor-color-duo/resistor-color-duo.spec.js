72b161303f7647b790b51bce297c5da45af37cca
Mario D'Andrea <goffredo2004@gmail.com>
2026-05-03 10:34:31 +0200

    update JavaScript: Resistor Color Duo

--- javascript/resistor-color-duo/resistor-color-duo.spec.js
+++ javascript/resistor-color-duo/resistor-color-duo.spec.js
@@ -1,23 +1,31 @@
-import { decodedValue } from './resistor-color-duo';
+import { decodedValue } from "./resistor-color-duo";
 
-describe('Resistor Colors', () => {
-  test('Brown and black', () => {
-    expect(decodedValue(['brown', 'black'])).toEqual(10);
+describe("Resistor Colors", () => {
+  test("Brown and black", () => {
+    expect(decodedValue(["brown", "black"])).toEqual(10);
   });
 
-  test('Blue and grey', () => {
-    expect(decodedValue(['blue', 'grey'])).toEqual(68);
+  test("Blue and grey", () => {
+    expect(decodedValue(["blue", "grey"])).toEqual(68);
   });
 
-  test('Yellow and violet', () => {
-    expect(decodedValue(['yellow', 'violet'])).toEqual(47);
+  test("Yellow and violet", () => {
+    expect(decodedValue(["yellow", "violet"])).toEqual(47);
   });
 
-  test('Orange and orange', () => {
-    expect(decodedValue(['orange', 'orange'])).toEqual(33);
+  test("White and red", () => {
+    expect(decodedValue(["white", "red"])).toEqual(92);
   });
 
-  test('Ignore additional colors', () => {
-    expect(decodedValue(['green', 'brown', 'orange'])).toEqual(51);
+  test("Orange and orange", () => {
+    expect(decodedValue(["orange", "orange"])).toEqual(33);
+  });
+
+  test("Ignore additional colors", () => {
+    expect(decodedValue(["green", "brown", "orange"])).toEqual(51);
+  });
+
+  test("Black and brown, one-digit", () => {
+    expect(decodedValue(["black", "brown"])).toEqual(1);
   });
 });
